b7482c45282dbd302ff2e5df39236225132cdcdd
talha2299 <sheikhtalha2299@gmail.com>
2024-05-28 16:57:19 +0500

    bugs fix

--- signup.js
+++ signup.js
@@ -1,50 +1,35 @@
+function sendOTP() {
+	const email = document.getElementById('email');
+	const otpverify = document.getElementsByClassName('otpverify')[0];
 
+	let otp_val = Math.floor(Math.random() * 10000);
 
-//Please Subscribe Ash_is_Coding.
+	let emailbody = `<h2>Your OTP is </h2>${otp_val}`;
+	Email.send({
+    SecureToken : "833ac428-d96c-417c-b3fa-ad8a9ed82f31",
+    To : email.value,
+    From : "sheikhtalha2299@gmail.com",
+    Subject : "Email OTP using JavaScript",
+    Body : emailbody,
+}).then(
 
-function sendOTP(){
-    const email = document.getElementById('email');
-    const otpverify = document.getElementsByClassName('otpverify')[0];
+	message => {
+		if (message === "OK") {
+			alert("OTP sent to your email " + email.value);
 
-    // Create a SMTP crendentials that I showed u in my previous video
+			otpverify.style.display = "flex";
+			const otp_inp = document.getElementById('otp_inp');
+			const otp_btn = document.getElementById('otp-btn');
 
-    // Generating random number as OTP;
-
-    let otp_val = Math.floor(Math.random()*10000);
-
-    let emailbody = `
-        <h1>Please Subscribe to Ash_is_Coding</h1> <br>
-        <h2>Your OTP is </h2>${otp_val}
-    `;
-
-    Email.send({
-        SecureToken : "7e2e5c6d-ca82-4381-9421-0e32e255c533",
-        To : email.value,
-        From : "sheikhtalha22999@gmail.com",
-        Subject : "This is the from Ash_is_Coding, Please Subscribe",
-        Body : emailbody
-    }).then(
-        //if success it returns "OK";
-      message => {
-        if(message === "OK"){
-            alert("OTP sent to your email "+email.value);
-
-            // now making otp input visible
-            otpverify.style.display = "block";
-            const otp_inp = document.getElementById('otp_inp');
-            const otp_btn = document.getElementById('otp-btn');
-
-            otp_btn.addEventListener('click',()=>{
-                // now check whether sent email is valid
-                if(otp_inp.value == otp_val){
-                    alert("Email address verified...");
-                }
-                else{
-                    alert("Invalid OTP");
-                }
-            })
-        }
-      }
-    );
-
+			otp_btn.addEventListener('click', () => {
+				if (otp_inp.value == otp_val) {
+					alert("Email address verified...");
+				}
+				else {
+					alert("Invalid OTP");
+				}
+			})
+		}
+	}
+);
 }
